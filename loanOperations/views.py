2f6ed8792448a3a65c7626af59f4d2ac01f08e2b
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:59:42 +0200

    importing the required library model and serializers to handle all operations of the request mapping of the client

--- loanOperations/views.py
+++ loanOperations/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
-# Create your views here.
+from django.shortcuts import render
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import LoanRequest, LoanInvestorOffers, LoanSubmit, LoanSchedule
+from borrower.models import Borrower
+from investor.models import Investor
+from investor.serializers import InvestorSerializer
+
+from .serializers import LoanRequestSerializer, LoanInvestoroffersSerializer, LoanSubmitSerializer, LoanScheduleSerializer
+
+
+
+
+
+
