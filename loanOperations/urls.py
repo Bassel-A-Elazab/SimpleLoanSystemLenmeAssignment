33f0dd1a80f2889f3fb515499a78019f9023f430
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 18:21:41 +0200

    set the url for the path of completed the loan request

--- loanOperations/urls.py
+++ loanOperations/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
 
     path('investor/<str:pk>/list_loan', views.detailRequestsForInvestor, name="list-loan-request"),
     path('investor/<str:pk_investor>/offer/<str:pk_loan>', views.createOfferLoan, name="Offer-loan-request"),
+
+    path('admin/submitted/<str:pk_borrower>/<str:pk_loan>/<str:pk_investor>/completed', views.completedPaymentBorrower, name="complete-loan-request"),  
 ]
