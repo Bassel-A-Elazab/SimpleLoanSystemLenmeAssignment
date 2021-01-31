217159575a7099a565902bbcd0508d534d7f7b49
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 09:51:25 +0200

    import object dosn't exisit for able to use in handling exceptions

--- investor/views.py
+++ investor/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
