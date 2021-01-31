d0be70651b84d18c8598549910604d2b3d7087d4
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:22:25 +0200

    calling the required library to use django-rest and just display all the urls of investor module

--- investor/views.py
+++ investor/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import InvestorSerializer
+from .models import Investor
+
 # Create your views here.
+@api_view(['GET'])
+def apiOverview1(request):
+	api_urls = {
+		'Create':'/Investor-create/',
+        'Detail Investor':'/Investor-detail/<str:pk>/',
+		'Update':'/Investor-update/<str:pk>/',
+		'Delete':'/Investor-delete/<str:pk>/',
+		}
+	return Response(api_urls)
+
