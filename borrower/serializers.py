020890d30d74d880e2bfff958c05262d89d6bc89
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 07:48:49 +0200

    creates a borrower serialization that can handle with json request over http protocol and other operations in django-rest

---
+++ borrower/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Borrower
+
+
+class BorrowerSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Borrower
+		fields ='__all__'
+
