b29d9f4e03c3afc7d471f0a320bb2ccd3b7b500d
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 07:47:17 +0200

    creates class borrower to handle the borrower module for all operation comes

--- borrower/models.py
+++ borrower/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Borrower(models.Model):
+    username = models.CharField(max_length=150)
+    mail = models.CharField(max_length=150)
+    fname = models.CharField(max_length=150)
+    lname = models.CharField(max_length=150)
+    phone = models.CharField(max_length=150)
