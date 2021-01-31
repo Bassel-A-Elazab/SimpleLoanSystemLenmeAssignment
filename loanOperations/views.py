045f6433ca554ddc42f2960b4d60b54360c4f0b0
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 09:02:58 +0200

    request mapping of borrower that want to retrieve all loan requests

--- loanOperations/views.py
+++ loanOperations/views.py
@@ -17,7 +17,16 @@ from investor.serializers import InvestorSerializer
 from .serializers import LoanRequestSerializer, LoanInvestoroffersSerializer, LoanSubmitSerializer, LoanScheduleSerializer
 
 
+# To display the all borrower loan requests
+@api_view(['GET'])
+def detailRequestBorrower(request, pk):
+	try:
+		borrower = Borrower.objects.get(id=pk)          # query borroer using ID.
+	except ObjectDoesNotExist:                          # if borrower dosn't exist
+		return Response("Sorry! You aren't register, Please create a new account", status=status.HTTP_401_UNAUTHORIZED)
 
-
-
-
+	loan_request = LoanRequest.objects.filter( Q(Id_borrower=pk)).values()  # query all request related to borrower
+	if(loan_request.exists()):
+		return Response(querySet_to_list(loan_request))
+	else:
+		return Response("There aren't loan requests yet", status=status.HTTP_404_NOT_FOUND)
