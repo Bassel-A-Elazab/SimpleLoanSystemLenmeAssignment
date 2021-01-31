e0dd14fc93a6ff5c4ea781fc21c0523bf830102e
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:36:06 +0200

    creates class model to store all loan request that's belonging to each borrower

--- loanOperations/models.py
+++ loanOperations/models.py
@@ -3,3 +3,11 @@ from borrower.models import Borrower
 from investor.models import Investor
 
 # Create your models here.
+
+# for handle all borrower loan requests and their status
+class LoanRequest(models.Model):
+    Id_borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    request_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=100, default="null")
+
