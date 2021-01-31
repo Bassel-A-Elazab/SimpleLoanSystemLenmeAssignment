82c54f6c598d7dfc9fcff4dc7bd3d2bbfffccaea
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:49:06 +0200

    creates serializations for all models that created in loan module to handle all operation of all processing

--- loanOperations/serializers.py
+++ loanOperations/serializers.py
@@ -5,3 +5,34 @@ from borrower.models import Borrower
 from borrower.serializers import BorrowerSerializer
 from investor.serializers import InvestorSerializer
 
+# for loan request module
+class LoanRequestSerializer(serializers.ModelSerializer):
+	Id_borrower = BorrowerSerializer(read_only=True)
+	
+	class Meta:	
+		model = LoanRequest
+		fields ='__all__'
+
+# for invesotr offer module
+class LoanInvestoroffersSerializer(serializers.ModelSerializer):
+	Id_investor = InvestorSerializer(read_only=True)
+	Id_loan_request = LoanRequestSerializer(read_only=True)
+	class Meta:	
+		model = LoanInvestorOffers
+		fields ='__all__'
+
+# for loan submitted module
+class LoanSubmitSerializer(serializers.ModelSerializer):
+	Id_borrower = BorrowerSerializer(read_only=True)
+	Id_investor = InvestorSerializer(read_only=True)
+	Id_loan_request = LoanRequestSerializer(read_only=True)
+	class Meta:	
+		model = LoanSubmit
+		fields ='__all__'
+
+# for scheduled module 
+class LoanScheduleSerializer(serializers.ModelSerializer):
+	Id_submit = LoanSubmitSerializer(read_only=True)
+	class Meta:	
+		model = LoanSchedule
+		fields ='__all__'
