c617f2dc1cdd5d409cc9178f65670ab724254684
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 09:35:46 +0200

    set the path for loan operations and include it for routing

--- LoanSystem/urls.py
+++ LoanSystem/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('borrower.urls')),
     path('api/', include('investor.urls')),
+    path('api/', include('loanOperations.urls')),
 ]
