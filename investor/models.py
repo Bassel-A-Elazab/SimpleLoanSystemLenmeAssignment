fe6e003199dacacfa2450d85e5887e24df07206c
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:17:09 +0200

    creates class model for investor to handle borrower operations

--- investor/models.py
+++ investor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Investor(models.Model):
+    username = models.CharField(max_length=150)
+    mail = models.CharField(max_length=150)
+    fname = models.CharField(max_length=150)
+    lname = models.CharField(max_length=150)
+    phone = models.CharField(max_length=150)
+    balance = models.FloatField()
+
