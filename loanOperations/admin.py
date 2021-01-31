3e97340317c1d6074d62000ddc3a587c989ced7a
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 08:54:23 +0200

    register all loan operations models in admin

--- loanOperations/admin.py
+++ loanOperations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import LoanRequest, LoanInvestorOffers, LoanSubmit, LoanSchedule
 # Register your models here.
+
+admin.site.register(LoanRequest)
+admin.site.register(LoanInvestorOffers)
+admin.site.register(LoanSubmit)
+admin.site.register(LoanSchedule)
+
