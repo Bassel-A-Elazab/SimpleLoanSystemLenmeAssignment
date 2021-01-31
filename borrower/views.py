c3860e79a7c2888bd09a12e7b1eb1ba418c50ef6
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 07:55:40 +0200

    import the required library to use django-rest my modeuls and my serializers and just creates my url view

--- borrower/views.py
+++ borrower/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Borrower
+from .serializers import BorrowerSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+	api_urls = {
+		'Create':'/borrower-create/',
+        'Detail borrower':'/borrower-detail/<str:pk>/',
+		'Update':'/borrower-update/<str:pk>/',
+		'Delete':'/borrower-delete/<str:pk>/',
+		}
+	return Response(api_urls)
