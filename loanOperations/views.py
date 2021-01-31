60f55266cdea8cfeb8a00253d8fb2fbf67722170
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-31 09:06:35 +0200

    maaping a request for borrower that want to loan money, And later invstor can offer

--- loanOperations/views.py
+++ loanOperations/views.py
@@ -22,11 +22,29 @@ from .serializers import LoanRequestSerializer, LoanInvestoroffersSerializer, Lo
 def detailRequestBorrower(request, pk):
 	try:
 		borrower = Borrower.objects.get(id=pk)          # query borroer using ID.
-	except ObjectDoesNotExist:                          # if borrower dosn't exist
+	except ObjectDoesNotExist:                          # if borrower dosn't exist.
 		return Response("Sorry! You aren't register, Please create a new account", status=status.HTTP_401_UNAUTHORIZED)
 
-	loan_request = LoanRequest.objects.filter( Q(Id_borrower=pk)).values()  # query all request related to borrower
+	loan_request = LoanRequest.objects.filter( Q(Id_borrower=pk)).values()  # query all request related to borrower.
 	if(loan_request.exists()):
 		return Response(querySet_to_list(loan_request))
 	else:
 		return Response("There aren't loan requests yet", status=status.HTTP_404_NOT_FOUND)
+
+#  Borrower request for loan.
+@api_view(['POST'])
+def loanRequest_create(request,pk):
+	try:
+		borrower = Borrower.objects.get(id=pk)      # check if borrower exist first.
+	except ObjectDoesNotExist:                      #handle exception if isn't exist and return 401 unauthoized
+		return Response("Sorry! You aren't register, Please create a new account", status=status.HTTP_401_UNAUTHORIZED)
+	
+	request.data["Id_borrower"] = borrower.id
+	loanRequest = LoanRequestSerializer(data=request.data)
+	
+	if loanRequest.is_valid():          # save a request of loan related with borrower that ask for it.
+		loanRequest.validated_data["Id_borrower_id"] =  borrower.id
+		loanRequest.save()
+		return Response("Successfully, Please waiting for an offer from one of the investors", status=status.HTTP_201_CREATED)
+	return Response("Sorry, Please Enter valid data", status=status.HTTP_400_BAD_REQUEST)
+
